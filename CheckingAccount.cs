8c8123f0150a9f4f323c94e83199da9500e4ef99
Mauricio Diaz <mrcdz@pm.me>
2021-11-16 21:21:37 -0700

    Complete Deposit, Withdraw and MonthEnd methods

--- CheckingAccount.cs
+++ CheckingAccount.cs
@@ -5,18 +5,20 @@ namespace Activity5._2._3
     public class CheckingAccount : Account
     {
         public int TransactionLimit { get; set; }
-        public int TransactionCount { get; private set; }
+        public int TransactionCount { get; private set; } = 0;
         public double TransactionFee { get; set; }
         public double AccountFee { get; set; }
 
         public override void Deposit(double amount)
         {
-           //ToDo
+           this.Value += amount;
+           this.TransactionCount++;
         }
 
         public override void Withdraw(double amount)
         {
-            //ToDo
+            this.Value -= amount;
+            this.TransactionCount++;
         }
 
         private void UpdateTransectionCount()
@@ -26,7 +28,8 @@ namespace Activity5._2._3
 
         public void MonthEnd()
         {
-            //ToDo
+            this.Value -= this.AccountFee;
+            this.TransactionCount = 0;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,28 @@ namespace Activity5._2._3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+            var mySavingsAccount = new SavingsAccount { ID = 5, Name = "My Savings Account", InterestRate = 0.01 };
+            mySavingsAccount.Deposit(100.0);
+            mySavingsAccount.Withdraw(20.0);
+            Console.WriteLine("Current Balance after all transections (Savings Account): "+mySavingsAccount.Value);
+            mySavingsAccount.MonthEnd();
+            Console.WriteLine("Balance after month end (Savings Account): " + mySavingsAccount.Value);
+            */
+            var myCheckingAccount = new CheckingAccount {ID = 7, Name = "My Checking Account", TransactionLimit = 2, TransactionFee = 1.5, AccountFee = 5.0};
+            myCheckingAccount.Deposit(100.0);
+            myCheckingAccount.Withdraw(10.0);
+            myCheckingAccount.Withdraw(10.0);
+            myCheckingAccount.Withdraw(10.0);
+            myCheckingAccount.Deposit(100.0);
+            myCheckingAccount.Withdraw(20.0);
+            Console.WriteLine("Current Balance after all transections (Checking Account): " + myCheckingAccount.Value);
+            myCheckingAccount.MonthEnd();
+            Console.WriteLine("Balance after month end (Checking Account):" + myCheckingAccount.Value);
+
+
+
+            Console.ReadKey();
         }
     }
 }
